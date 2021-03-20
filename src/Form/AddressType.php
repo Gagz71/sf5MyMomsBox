5071535905c6b21762d26d3f9b4f9e4ae0f641af
Doun Douns <sangodoun@protonmail.com>
2021-03-20 20:54:00 +0100

    css gestion d'adresses

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -95,7 +95,7 @@ class AddressType extends AbstractType
             ->add('submit', SubmitType::class, [
                 'label'=>'Valider mon adresse',
                 'attr' => [
-                    'class' => 'button foat-right delivery-btn'
+                    'class' => 'button float-right delivery-btn'
                 ]
             ])
         ;
